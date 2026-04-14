7a8cecd930038b6126248bda72cb84f01691464b
barca <barca@example.com>
2026-04-14 10:55:08 -0600

    fix(build): externalize pdfmake for netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    eslint: {
-        ignoreDuringBuilds: true
-    }
+    serverExternalPackages: ['pdfmake', 'jszip']
 };
 
 export default nextConfig;
